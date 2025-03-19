2624bb54581893d20808bc005f4a2d6b4f8fe739
Nicolas Diovani <nicolasdiovanioliveira@gmail.com>
2025-03-18 21:42:34 -0300

    18.03.25 21:42

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    struct Pessoa
+    {
+        public string nome;
+        public int idade;
+        public double altura;
+        //mostrar(string nome, string idade)
+        //{
+            
+        //}
+        public string Retorno(string nome, int idade)
+        {
+            string junto = nome + idade.ToString();
+            Console.WriteLine(junto);
+            return junto;
+        }
+    }
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa();
+        p1.nome = "nicolas";
+        p1.altura = 1.74;
+        p1.idade = 18;
+        Pessoa p2 = new Pessoa()
+        {
+            nome="antonio",
+            idade=13,
+            altura=1.45
+        };
+        Console.WriteLine(p1.Retorno("nicolas", 19));
+    }
+}
