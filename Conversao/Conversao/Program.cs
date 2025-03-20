a7e3331c69604e9a2730db6781b076331781cfe7
Nicolas Diovani <nicolasdiovanioliveira@gmail.com>
2025-03-19 22:00:47 -0300

    19.03.25 22:00

---
+++ Conversao/Conversao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1 = 100; // 8 bits, de 0 a 255
+            ushort num2; // 16 bits, 0 a 65.535
+            //conversao explicita:
+            float num3 = 100f;
+            int num4 = (int)num3;
+            //metodo parse
+            string txt = "2984";
+            int numero = int.Parse(txt);
+        }
+    }
+}
